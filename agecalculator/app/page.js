19ee62125982f8ce876ff4430bcd459ce4ceb707
Your Name <you@example.com>
2023-11-02 16:34:46 +0300

    first component

--- agecalculator/app/page.js
+++ agecalculator/app/page.js
@@ -2,7 +2,7 @@ import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className=" flex justify-center bg-slate-300 max-h-screen max-w-max text-center">
+    <main className="flex justify-center items-center bg-slate-300  h-screen w-screen">
       <Hero />
     </main>
   );
--- agecalculator/components/hero.jsx
+++ agecalculator/components/hero.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="bg-white">
+    <div className="bg-white w-screen ms-3 me-3 h-3/5 -mt-[5rem] rounded-t-3xl rounded-bl-3xl rounded-br-[7rem]">
       <div>Hello</div>
+      <div>hello</div>
     </div>
   );
 };
